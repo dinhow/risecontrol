f74e4e6a2c40da4c762ae2c7b37a49da21f2ff6f
Edmilson <dinhow.exe@gmail.com>
2022-08-16 21:56:52 -0300

    Pronto para deploy

--- risecontrol/settings.py
+++ risecontrol/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 import datetime
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-ri*vw!w7t+dx-ih9_7re(1gkd6^j&56o+f7#(_fd)y)9u(wlyv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://rise-control-api.herokuapp.com/']
 
 
 # Application definition
@@ -151,3 +152,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
