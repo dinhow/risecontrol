6054496b6f0b57f230f1613671e885b3e39633ac
Edmilson <dinhow.exe@gmail.com>
2022-08-17 23:21:06 -0300

    Pronto para deploy

--- user/api/serializers.py
+++ user/api/serializers.py
@@ -4,7 +4,7 @@ from ..models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('name', 'userName', 'password')
+        fields = "__all__"
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
